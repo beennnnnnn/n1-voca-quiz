89f74d628a0ae2d5d15389b046d9a237131de5b9
beennnnnnn <58132460+beennnnnnn@users.noreply.github.com>
2026-03-14 22:11:38 +0900

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("🔥 나만의 세법 & N1 단어 박살내기")
 # ---------------------------------------------------------
 # 본인의 구글 시트 URL을 확인하세요! (캡처해주신 시트 기준)
 # ---------------------------------------------------------
-SHEET_URL = "https://docs.google.com/spreadsheets/d/여기에_본인의_시트ID를_넣으세요/export?format=csv"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1g6ww8VWi7PytD5oH6iGMcUfGc0P-0uGeC4CKGEEGbU0/export?format=csv"
 
 # 2. 구글 시트 데이터 로드
 @st.cache_data(ttl=60)
